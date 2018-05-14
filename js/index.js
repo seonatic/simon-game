a9e6d33490d57de11de984a4055b164a1a25d7ac
CESAR NAVARRO <34008442+seonatic@users.noreply.github.com>
2018-05-14 13:02:37 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,178 @@
+ var random = [];
+        var litID = [];
+        var lit;
+        var clicked = [];
+        var i;
+        var j = 0;
+        var count = 1;
+        var on;
+        var off;
+        var strict;
+        var x;
+
+
+        function change1() {
+
+            //set interval length
+            if (count <= 10) {
+                off = 400;
+                on = 800;
+            } else {
+                off = 250;
+                on = 500;
+            }
+            x = setInterval(function () {
+
+                if (random[j] == 1) {
+                    lit = 'light1';
+                    $('#one').addClass(lit);
+                    $('#audio1')[0].play();
+                    litID.push(1);
+                    setTimeout(function () {
+                        $('#one').removeClass(lit);
+                    }, off);
+
+                } else if (random[j] == 2) {
+                    lit = 'light2';
+                    $('#two').addClass(lit);
+                    $('#audio2')[0].play();
+                    litID.push(2);
+                    setTimeout(function () {
+                        $('#two').removeClass(lit);
+                    }, off);
+                } else if (random[j] == 3) {
+                    lit = 'light3';
+                    $('#three').addClass(lit);
+                    $('#audio3')[0].play();
+                    litID.push(3);
+                    setTimeout(function () {
+                        $('#three').removeClass(lit);
+                    }, off);
+                } else {
+                    lit = 'light4';
+                    $('#four').addClass(lit);
+                    $('#audio4')[0].play();
+                    litID.push(4);
+                    setTimeout(function () {
+                        $('#four').removeClass(lit);
+                    }, off);
+                }
+
+                j++;
+
+                if (j >= count) {
+                    clearInterval(x);
+                }
+
+            }, on);
+
+        }
+
+
+        function checking() {
+
+            if (litID.length == clicked.length) {
+                if (litID.join() == clicked.join()) {
+                    if (count == 20) {
+                       
+                        setTimeout(function () {
+                          $('#count').text("You Win");
+                          history.go(0);
+                        }, 1200);
+                      
+                    } else {
+                        setTimeout(function () {
+                            $('#count').text(count + 1);
+                            count++;
+                            litID = [];
+                            clicked = [];
+                            j = 0;
+                            change1();
+                        }, 1000);
+                    }
+                } else {
+                    if (strict == 1) {
+                        history.go(0);
+                    } else {
+                        setTimeout(function () {
+                            $('#count').text(' Wrong! Try Again');
+                            litID = [];
+                            clicked = [];
+                            j = 0;
+                            change1();
+                        }, 1000);
+                    }
+                }
+            }
+        }
+
+
+        //On Button
+        $('#on').on('click', function () {
+            $('#count').text('--');
+            for (i = 0; i < 20; i++) {
+                random[i] = Math.ceil((Math.random() * 4));
+            }
+
+            $('#strict').on('click', function () {
+                strict = 1;
+                
+            });
+
+            //Start Button 
+            $('#start').on('click', function () {
+                $('#count').text(count);
+                change1();
+
+            });
+
+            //Player turn, stores values inside clicked array to compare afterwards with rhe Lid Array through the function check
+            $('#one').on('click', function () {
+                $('#one').addClass('light1');
+                $('#audio1')[0].play();
+                clicked.push(1);
+                setTimeout(function () {
+                    $('#one').removeClass('light1');
+                }, 250);
+
+                checking();
+            });
+
+            $('#two').on('click', function () {
+                $('#two').addClass('light2');
+                $('#audio2')[0].play();
+                clicked.push(2);
+                setTimeout(function () {
+                    $('#two').removeClass('light2');
+                }, 250);
+
+                checking();
+            });
+
+            $('#three').on('click', function () {
+                $('#three').addClass('light3');
+                $('#audio3')[0].play();
+                clicked.push(3);
+                setTimeout(function () {
+                    $('#three').removeClass('light3');
+                }, 250);
+
+                checking();
+            });
+
+            $('#four').on('click', function () {
+                $('#four').addClass('light4');
+                $('#audio4')[0].play();
+                clicked.push(4);
+                setTimeout(function () {
+                    $('#four').removeClass('light4');
+                }, 250);
+
+                checking();
+            });
+        });
+
+        //to switch off
+        $('#off').on('click', function () {
+            history.go(0);
+        });
